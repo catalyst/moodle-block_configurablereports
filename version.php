5c72cfbf9713ee12174c8f620a8beb0835d3a895
Jay Oswald <jozzie52@gmail.com>
2026-09-08 17:46:04 +1000

    bump

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026062300;
+$plugin->version = 2026062301;
 $plugin->requires = 2017111300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '4.1.0';
